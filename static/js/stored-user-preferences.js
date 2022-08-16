3ec7a783f854deb4416e05b23c70c0717f2e8071
Zach Lasiuk <zach.lasiuk@arm.com>
2022-08-16 12:14:46 -0500

    added propogation of user preferences in theme, width, and height

---
+++ static/js/stored-user-preferences.js
@@ -0,0 +1,25 @@
+(() => {
+      // Always check local storage to keep these attributes constant:
+        // Theme  (light, dark)
+        // Width  (is-full-width)
+        // Height (expanded-masthead and contextual data)
+    	//Check Storage. Keep user preference on page reload
+
+	if (localStorage.getItem('lightMode')) {
+        document.querySelector('html').setAttribute('theme', 'light');
+	} 
+  if (localStorage.getItem('fullWidth')) {
+        document.getElementById("all-content-div").classList.add("is-full-width");
+	} 
+  if (localStorage.getItem('fullHeight')) {
+    document.getElementById('global-nav-example-default').contextualData = []; // Hide seoncary nav on Global Nav         
+    document.getElementById("expanded-masthead").setAttribute('hidden',true);  // Hide title
+    document.getElementById("arm-footer").setAttribute('hidden',true);         // Hide footer
+  } 
+
+
+  })();
+
+
+
+
